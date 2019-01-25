afe476d445901af8c632bb22c3f6aea58cf5d1fb
RainbowDashLabs <mail@code-fox.de>
2019-01-26 00:54:41 +0100

    add command to see a naked message

---
+++ src/main/java/de/chojo/shepard/modules/commands/util/GetRaw.java
@@ -0,0 +1,19 @@
+package de.chojo.shepard.modules.commands.util;
+
+import de.chojo.shepard.messageHandler.Messages;
+import de.chojo.shepard.modules.commands.Command;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class GetRaw extends Command {
+
+    public GetRaw(){
+        commandName = "getRaw";
+        commandDesc = "Get the message in raw format";
+    }
+
+    @Override
+    public boolean execute(String[] args, MessageReceivedEvent receivedEvent) {
+        Messages.sendMessage("`" + receivedEvent.getMessage().getContentRaw() + "`", receivedEvent.getChannel());
+        return true;
+    }
+}
