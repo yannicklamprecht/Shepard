a5410b76996960d1191c1cbe3212992e2ab26256
RainbowDashLabs <mail@code-fox.de>
2019-01-25 16:47:43 +0100

    add start message notify

--- src/main/java/de/chojo/shepard/listener/LogListener.java
+++ src/main/java/de/chojo/shepard/listener/LogListener.java
@@ -1,13 +1,28 @@
 package de.chojo.shepard.listener;
 
 import de.chojo.shepard.Collections.ServerCollection;
-import de.chojo.shepard.Messages;
+import de.chojo.shepard.messageHandler.Messages;
+import de.chojo.shepard.ShepardBot;
+import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
 
 public class LogListener extends ListenerAdapter {
     @Override
     public void onGuildJoin(GuildJoinEvent event) {
         Messages.sendMessage("Shepard ist nun auf " + event.getGuild().getName() + " verfügbar!", ServerCollection.getNormandy().getTextChannelById("538094461381640192"));
+    }
+
+    @Override
+    public void onReady(ReadyEvent event) {
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+        Date date = new Date(System.currentTimeMillis());
+        Messages.sendMessage("`[" + formatter.format(date) + "]` Shepard meldet sich zum Dienst! Erwarte ihre Befehle! Derzeit stehe ich auf " + ShepardBot.getJDA().getGuilds().size() + " Servern zur Verfügung!",
+                ServerCollection.getNormandy().getTextChannelById("538094461381640192"));
     }
 }
