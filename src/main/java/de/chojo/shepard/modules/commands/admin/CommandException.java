989c7bc7f625f9b403a477c31251e8983bcac8ea
RainbowDashLabs <mail@code-fox.de>
2019-01-26 00:53:26 +0100

    add new fancy exception

---
+++ src/main/java/de/chojo/shepard/modules/commands/admin/CommandException.java
@@ -0,0 +1,8 @@
+package de.chojo.shepard.modules.commands.admin;
+
+public class CommandException extends IllegalArgumentException {
+
+    public CommandException(String message) {
+        super(message);
+    }
+}
