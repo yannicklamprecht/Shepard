0cc895af742dc03523c924c156c0ebe9e05698b7
RainbowDashLabs <mail@code-fox.de>
2019-08-22 15:19:08 +0200

    add helper for context handling

---
+++ src/main/java/de/chojo/shepard/contexts/ContextHelper.java
@@ -0,0 +1,25 @@
+package de.chojo.shepard.contexts;
+
+import de.chojo.shepard.collections.CommandCollection;
+import de.chojo.shepard.collections.KeyWordCollection;
+import de.chojo.shepard.contexts.commands.Command;
+import de.chojo.shepard.contexts.keywords.Keyword;
+import de.chojo.shepard.messagehandler.Messages;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public final class ContextHelper {
+    public static String getContextName(String name, MessageReceivedEvent receivedEvent) {
+        Command command = CommandCollection.getInstance().getCommand(name);
+        Keyword keyword = KeyWordCollection.getInstance().getKeywordWithContextName(name, receivedEvent);
+
+        String contextName = null;
+        if (keyword != null) {
+            contextName = keyword.getClass().getSimpleName();
+        } else if (command != null) {
+            contextName = command.getClass().getSimpleName();
+        }
+
+        return contextName;
+    }
+
+}
