e16c5eabeb81a70d744a7a20968a2ec7969c8287
RainbowDashLabs <mail@code-fox.de>
2019-08-26 00:16:30 +0200

    add ticket showtypes command

--- src/main/java/de/eldoria/shepard/contexts/commands/admin/Ticket.java
+++ src/main/java/de/eldoria/shepard/contexts/commands/admin/Ticket.java
@@ -2,8 +2,13 @@ package de.eldoria.shepard.contexts.commands.admin;
 
 import de.eldoria.shepard.contexts.commands.Command;
 import de.eldoria.shepard.contexts.commands.CommandArg;
+import de.eldoria.shepard.database.queries.Tickets;
+import de.eldoria.shepard.database.types.TicketType;
 import de.eldoria.shepard.messagehandler.MessageSender;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.List;
 
 import static java.lang.System.lineSeparator;
 
@@ -42,6 +47,43 @@ public class Ticket extends Command {
         }
 
         if (cmd.equalsIgnoreCase("showTypes")) {
+            List<TicketType> tickets = Tickets.getTypes(receivedEvent.getGuild(), receivedEvent);
+            if (tickets.size() == 0) {
+                MessageSender.sendMessage("No ticket types defined", receivedEvent.getChannel());
+                return;
+            }
+
+            String categoryName = "Category Name";
+            int categoryNameSize = categoryName.length();
+            String typeKeyword = "Type/Keyword ";
+            int typeKeywordSize = typeKeyword.length();
+
+            for (TicketType type : tickets) {
+                if (type.getCategory() == null) {
+                    continue;
+                }
+                int categoryLength = type.getCategory().getName().length();
+                if (categoryLength > categoryNameSize) {
+                    categoryNameSize = categoryLength;
+                }
+
+                if (type.getKeyword().length() + 1 > typeKeywordSize) {
+                    typeKeywordSize = type.getKeyword().length();
+                }
+            }
+
+            StringBuilder builder = new StringBuilder();
+            builder.append("**__Ticket Types with Categories:__**").append(lineSeparator())
+                    .append("```yaml")
+                    .append(StringUtils.rightPad(typeKeyword, typeKeywordSize, " "))
+                    .append(categoryName).append(lineSeparator());
+            for (TicketType type : tickets) {
+                builder.append(StringUtils.rightPad(type.getKeyword(), typeKeywordSize, " "))
+                        .append(type.getCategory().getName()).append(lineSeparator());
+            }
+            builder.append("```");
+
+            MessageSender.sendMessage(builder.toString(), receivedEvent.getChannel());
             return;
         }
         MessageSender.sendSimpleError("Invalid Argument", receivedEvent.getChannel());
