bd8abb885ced716721ec03b962fe1360493b0077
RainbowDashLabs <mail@code-fox.de>
2019-08-29 01:20:50 +0200

    add listener to remove channels when a ticket owner leaves the guild

---
+++ src/main/java/de/eldoria/shepard/listener/TicketCleanupListener.java
@@ -0,0 +1,27 @@
+package de.eldoria.shepard.listener;
+
+import de.eldoria.shepard.util.Verifier;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberLeaveEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import javax.annotation.Nonnull;
+
+import java.util.List;
+
+import static de.eldoria.shepard.database.queries.Tickets.getChannelIdsByOwner;
+import static de.eldoria.shepard.database.queries.Tickets.removeChannel;
+
+public class TicketCleanupListener extends ListenerAdapter {
+    @Override
+    public void onGuildMemberLeave(@Nonnull GuildMemberLeaveEvent event) {
+        List<String> channelIds = getChannelIdsByOwner(event.getGuild(), event.getUser(), null);
+
+        List<TextChannel> validTextChannels = Verifier.getValidTextChannels(event.getGuild(), channelIds);
+        for (TextChannel channel : validTextChannels) {
+            removeChannel(event.getGuild(), channel, null);
+            channel.delete().queue();
+        }
+
+    }
+}
