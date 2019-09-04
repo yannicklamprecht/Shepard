da46566f3117618351734c4c0fddc01caaa86a65
RainbowDashLabs <mail@code-fox.de>
2019-09-04 22:18:00 +0200

    removed database password from log

--- src/main/java/de/eldoria/shepard/database/DatabaseConnector.java
+++ src/main/java/de/eldoria/shepard/database/DatabaseConnector.java
@@ -33,8 +33,8 @@ public final class DatabaseConnector {
                     config.getAddress(), config.getPort(), config.getDb()),
                     config.getUsername(), config.getPassword());
             ShepardBot.getLogger().info(
-                    String.format("SQL connection established on %s:%s@%s:%s/%s",
-                            config.getUsername(), config.getPassword(), config.getAddress(),
+                    String.format("SQL connection established on %s:*******@%s:%s/%s",
+                            config.getUsername(), config.getAddress(),
                             config.getPort(), config.getDb()));
         } catch (SQLException ex) {
             handleException(ex, null);
