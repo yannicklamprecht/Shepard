f17edb099bc5790ccfaa79e37b72fd0e8239f9ef
Hannes Greule <hannesgreule@outlook.de>
2019-09-28 00:50:40 +0200

    Improve readability

--- src/main/java/de/eldoria/shepard/contexts/commands/fun/MockingSpongebob.java
+++ src/main/java/de/eldoria/shepard/contexts/commands/fun/MockingSpongebob.java
@@ -1,7 +1,10 @@
 package de.eldoria.shepard.contexts.commands.fun;
 
 import de.eldoria.shepard.contexts.commands.Command;
+import de.eldoria.shepard.messagehandler.ErrorType;
+import de.eldoria.shepard.messagehandler.MessageSender;
 import de.eldoria.shepard.wrapper.MessageEventDataWrapper;
+import net.dv8tion.jda.api.entities.MessageChannel;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,6 +13,7 @@ import java.net.URL;
 import java.util.UUID;
 
 public class MockingSpongebob extends Command {
+    private static final String BASE_URL = "https://mockingspongebob.org/";
 
     public MockingSpongebob() {
         this.commandName = "mockingSpongebob";
@@ -20,22 +24,20 @@ public class MockingSpongebob extends Command {
     @Override
     protected void internalExecute(String label, String[] args, MessageEventDataWrapper messageContext) {
         if (args.length == 0) {
-            messageContext.getChannel().sendMessage("No text given. What's wrong with you?").queue();
+            MessageSender.sendSimpleError(ErrorType.TOO_FEW_ARGUMENTS, messageContext.getChannel());
             return;
         }
-        String urlString = "https://mockingspongebob.org/" + String.join("%20", args);
         try {
-            HttpURLConnection connection = (HttpURLConnection) new URL(urlString).openConnection();
-            InputStream inputStream = connection.getInputStream();
-            messageContext.getChannel().sendFile(inputStream, "mock" + UUID.randomUUID() + ".jpg").queue(message -> {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
+            sendImage(String.join("%20", args), messageContext.getChannel());
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void sendImage(String text, MessageChannel channel) throws IOException {
+        String urlString = BASE_URL + text;
+        HttpURLConnection connection = (HttpURLConnection) new URL(urlString).openConnection();
+        InputStream inputStream = connection.getInputStream();
+        channel.sendFile(inputStream, "mock" + UUID.randomUUID() + ".jpg").queue();
     }
 }
