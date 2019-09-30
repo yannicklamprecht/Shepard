c06c41325495ef0c3a9a85ace70244a6a3b08c60
RainbowDashLabs <mail@code-fox.de>
2019-09-30 17:37:08 +0200

    add constructor with default embed value

--- src/main/java/de/eldoria/shepard/messagehandler/ErrorType.java
+++ src/main/java/de/eldoria/shepard/messagehandler/ErrorType.java
@@ -10,91 +10,91 @@ public enum ErrorType {
     /**
      * Used when a command argument is not valid.
      */
-    INVALID_ARGUMENT("Invalid argument!", false),
+    INVALID_ARGUMENT("Invalid argument!"),
     /**
      * Used when a command action is not valid.
      */
-    INVALID_ACTION("Invalid action!", false),
+    INVALID_ACTION("Invalid action!"),
     /**
      * Used when too many arguments are passed.
      */
-    TOO_MANY_ARGUMENTS("Too many arguments!", false),
+    TOO_MANY_ARGUMENTS("Too many arguments!"),
     /**
      * Used when too few arguments are passed.
      */
-    TOO_FEW_ARGUMENTS("Too few arguments!", false),
+    TOO_FEW_ARGUMENTS("Too few arguments!"),
     /**
      * Used when a number parse failed.
      */
-    NOT_A_NUMBER("This is not a number!", false),
+    NOT_A_NUMBER("This is not a number!"),
     /**
      * Used when a channel is not a guild text channel.
      */
-    NOT_GUILD_TEXT_CHANNEL("This is not a guild text channel!", false),
+    NOT_GUILD_TEXT_CHANNEL("This is not a guild text channel!"),
     /**
      * Used when a category was passed but not found.
      */
-    INVALID_CATEGORY("This is not a valid category!", false),
+    INVALID_CATEGORY("This is not a valid category!"),
     /**
      * Used when a channel was passed but not found.
      */
-    INVALID_CHANNEL("This is not a valid channel!", false),
+    INVALID_CHANNEL("This is not a valid channel!"),
     /**
      * Used when a role was passed but not found.
      */
-    INVALID_ROLE("This is not a valid role!", false),
+    INVALID_ROLE("This is not a valid role!"),
     /**
      * Used when a user was passed but not found.
      */
-    INVALID_USER("This is not a valid user!", false),
+    INVALID_USER("This is not a valid user!"),
     /**
      * Used when a id was found but the id is out of range.
      */
-    INVALID_ID("This is not a valid id!", false),
+    INVALID_ID("This is not a valid id!"),
     /**
      * Used when the prefix is to long.
      */
-    INVALID_PREFIX_LENGTH("Invalid prefix length. Only one or two Chars are allowed as prefix!", false),
+    INVALID_PREFIX_LENGTH("Invalid prefix length. Only one or two Chars are allowed as prefix!"),
     /**
      * Used when a context was not found while command parsing.
      */
-    INVALID_CONTEXT("Invalid Context!", false),
+    INVALID_CONTEXT("Invalid Context!"),
     /**
      * Used when a boolean could not be parsed.
      */
-    INVALID_BOOLEAN("Invalid input! Only 'true' and 'false' are valid inputs!", false),
+    INVALID_BOOLEAN("Invalid input! Only 'true' and 'false' are valid inputs!"),
     /**
      * Used when the list type could not be parsed.
      */
-    INVALID_LIST_TYPE("Invalid Input. Only 'blacklist' or 'whitelist are valid inputs", false),
+    INVALID_LIST_TYPE("Invalid Input. Only 'blacklist' or 'whitelist are valid inputs"),
     /**
      * Used when no message was found.
      */
-    NO_MESSAGE_FOUND("No message found!", false),
+    NO_MESSAGE_FOUND("No message found!"),
     /**
      * Used when no invite was found while registering.
      */
-    NO_INVITE_FOUND("No invite found with this code!", false),
+    NO_INVITE_FOUND("No invite found with this code!"),
     /**
      * Used when no quote was found.
      */
-    NO_QUOTE_FOUND("No quote found!", false),
+    NO_QUOTE_FOUND("No quote found!"),
     /**
      * Used when a ticket type is already defined.
      */
-    TYPE_ALREADY_DEFINED("This type is already defined!", false),
+    TYPE_ALREADY_DEFINED("This type is already defined!"),
     /**
      * Used when a ticket type is not found.
      */
-    TYPE_NOT_FOUND("Ticket type not found!", false),
+    TYPE_NOT_FOUND("Ticket type not found!"),
     /**
      * Used when someone tries to open a ticket for himself.
      */
-    TICKET_SELF_ASSIGNMENT("You can't open a ticket for yourself!", false),
+    TICKET_SELF_ASSIGNMENT("You can't open a ticket for yourself!"),
     /**
      * Used when a ticket close command is executed in a non ticket channel.
      */
-    NOT_TICKET_CHANEL("This is not a ticket channel!", false),
+    NOT_TICKET_CHANEL("This is not a ticket channel!"),
     /**
      * Used when a context is not found.
      */
@@ -102,7 +102,7 @@ public enum ErrorType {
     /**
      * Used when no emote was found.
      */
-    NO_EMOTE_FOUND("No emote was found.", false);
+    NO_EMOTE_FOUND("No emote was found.");
 
 
     /**
@@ -122,5 +122,9 @@ public enum ErrorType {
             this.message = "**ERROR**" + System.lineSeparator() + message;
         }
         this.isEmbed = embed;
+    }
+
+    ErrorType(String message) {
+        this(message, false);
     }
 }
