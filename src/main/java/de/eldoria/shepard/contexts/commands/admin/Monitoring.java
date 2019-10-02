24b2d74cab791a278aa91f544231cbdd0b2f45ce
RainbowDashLabs <mail@code-fox.de>
2019-10-02 15:51:45 +0200

    splitted into methods

--- src/main/java/de/eldoria/shepard/contexts/commands/admin/Monitoring.java
+++ src/main/java/de/eldoria/shepard/contexts/commands/admin/Monitoring.java
@@ -7,6 +7,7 @@ import de.eldoria.shepard.database.queries.MonitoringData;
 import de.eldoria.shepard.database.types.Address;
 import de.eldoria.shepard.messagehandler.ErrorType;
 import de.eldoria.shepard.messagehandler.MessageSender;
+import de.eldoria.shepard.util.AddressType;
 import de.eldoria.shepard.util.BooleanState;
 import de.eldoria.shepard.util.TextFormatting;
 import de.eldoria.shepard.util.Verifier;
@@ -36,9 +37,9 @@ public class Monitoring extends Command {
                                 + lineSeparator()
                                 + "**remove** -> [index] Enter index from show command" + lineSeparator()
                                 + "**show** -> leave empty." + lineSeparator()
-                                + "**activate** -> [channel] leave empty to set the current channel"
+                                + "**enable** -> [channel] leave empty to set the current channel"
                                 + " or enter channel for monitoring" + lineSeparator()
-                                + "**deactivate** -> leave empty" + lineSeparator(), true)
+                                + "**disable** -> leave empty" + lineSeparator(), false)
 
         };
     }
@@ -53,43 +54,17 @@ public class Monitoring extends Command {
         String cmd = args[0];
 
         if (Verifier.isArgument(cmd, "disable", "d")) {
-            MonitoringData.removeMonitoringChannel(messageContext.getGuild(), messageContext);
-            MessageSender.sendMessage("Monitoring Channel removed!", messageContext.getChannel());
+            disable(messageContext);
             return;
         }
 
         if (Verifier.isArgument(cmd, "show", "s")) {
-            List<Address> monitoringAddresses = MonitoringData.getMonitoringAddresses(
-                    messageContext.getGuild(), messageContext);
-
-            String[][] preparedStringTable = TextFormatting.getPreparedStringTable(monitoringAddresses,
-                    "Index", "Name", "Address", "Minecraft");
-
-            for (int i = 1; i < preparedStringTable.length; i++) {
-                Address address = monitoringAddresses.get(i);
-                preparedStringTable[i] = new String[] {
-                        address.getId() + "",
-                        address.getName(),
-                        address.getAddress(),
-                        TextFormatting.mapBooleanTo(address.isMinecraftIp(), "yes", "no")};
-            }
-
-            String asTable = TextFormatting.getAsTable(preparedStringTable);
-
-            MessageSender.sendMessage("Registered for Monitoring: " + lineSeparator()
-                            + "```" + lineSeparator()
-                            + asTable + lineSeparator()
-                            + "```",
-                    messageContext.getChannel());
+            show(messageContext);
+            return;
         }
 
         if (Verifier.isArgument(cmd, "enable", "e")) {
-            if (MonitoringData.setMonitoringChannel(messageContext.getGuild(),
-                    messageContext.getTextChannel(), messageContext)) {
-                MessageSender.sendMessage(
-                        "Registered " + messageContext.getTextChannel().getAsMention() + " as monitoring Channel",
-                        messageContext.getChannel());
-            }
+            if (easyEnable(messageContext)) return;
         }
 
         if (args.length < 2) {
@@ -98,62 +73,115 @@ public class Monitoring extends Command {
         }
 
         if (Verifier.isArgument(cmd, "remove", "r")) {
-            try {
-                int i = Integer.parseInt(args[1]);
-                if (MonitoringData.removeMonitoringAddressByIndex(messageContext.getGuild(), i, messageContext)) {
-                    MessageSender.sendMessage("Removed address with index: " + i, messageContext.getChannel());
-                }
-            } catch (NumberFormatException e) {
-                MessageSender.sendSimpleError(ErrorType.NOT_A_NUMBER, messageContext.getChannel());
-            }
+            remove(args[1], messageContext);
             return;
         }
 
         if (Verifier.isArgument(cmd, "enable", "e")) {
-            if (Verifier.isValidId(args[1])) {
-                TextChannel channel = messageContext.getGuild().getTextChannelById(DbUtil.getIdRaw(args[1]));
-                if (channel != null) {
-                    if (MonitoringData.setMonitoringChannel(messageContext.getGuild(), channel, messageContext)) {
-                        MessageSender.sendMessage(
-                                "Registered " + channel.getAsMention() + " as monitoring Channel",
-                                messageContext.getChannel());
-                    }
-                } else {
-                    MessageSender.sendSimpleError(ErrorType.INVALID_CHANNEL, messageContext.getChannel());
-                    return;
-                }
-            } else {
-                MessageSender.sendSimpleError(ErrorType.INVALID_ID, messageContext.getChannel());
-            }
+            if (enable(args[1], messageContext)) return;
             return;
         }
 
         if (Verifier.isArgument(cmd, "add", "a")) {
-            if (args.length != 3) {
-                MessageSender.sendSimpleError(ErrorType.INVALID_ARGUMENT, messageContext.getChannel());
-                return;
-            }
-            if (!Verifier.isAddress(args[1])) {
-                MessageSender.sendSimpleError(ErrorType.INVALID_ADDRESS, messageContext.getChannel());
-                return;
-            }
-
-            BooleanState booleanState = Verifier.checkAndGetBoolean(args[args.length - 1]);
-            if (booleanState == BooleanState.UNDEFINED) {
-                MessageSender.sendSimpleError(ErrorType.INVALID_BOOLEAN, messageContext.getChannel());
-                return;
-            }
-
-            String name = TextFormatting.getRangeAsString(" ", args, 2, args.length - 2);
-            if (MonitoringData.addMonitoringAddress(
-                    messageContext.getGuild(), args[1], name, booleanState.stateAsBoolean, messageContext)) {
-                MessageSender.sendMessage("Added Address **" + args[1] + "** as **" + name + "**!",
-                        messageContext.getTextChannel());
-            }
+            add(args, messageContext);
+            return;
         }
 
         MessageSender.sendSimpleError(ErrorType.INVALID_ACTION, messageContext.getChannel());
 
 
+    }
+
+    private boolean add(String[] args, MessageEventDataWrapper messageContext) {
+        if (args.length < 3) {
+            MessageSender.sendSimpleError(ErrorType.INVALID_ARGUMENT, messageContext.getChannel());
+            return true;
+        }
+        if (Verifier.getAddressType(args[1]) == AddressType.NONE) {
+            MessageSender.sendSimpleError(ErrorType.INVALID_ADDRESS, messageContext.getChannel());
+            return true;
+        }
+
+        BooleanState booleanState = Verifier.checkAndGetBoolean(args[args.length - 1]);
+        if (booleanState == BooleanState.UNDEFINED) {
+            MessageSender.sendSimpleError(ErrorType.INVALID_BOOLEAN, messageContext.getChannel());
+            return true;
+        }
+
+        String name = TextFormatting.getRangeAsString(" ", args, 2, args.length - 1);
+        if (MonitoringData.addMonitoringAddress(
+                messageContext.getGuild(), args[1], name, booleanState.stateAsBoolean, messageContext)) {
+            MessageSender.sendMessage("Registered Address **" + args[1] + "** as **" + name + "**!",
+                    messageContext.getTextChannel());
+        }
+        return false;
+    }
+
+    private boolean enable(String arg, MessageEventDataWrapper messageContext) {
+        if (Verifier.isValidId(arg)) {
+            TextChannel channel = messageContext.getGuild().getTextChannelById(DbUtil.getIdRaw(arg));
+            if (channel != null) {
+                if (MonitoringData.setMonitoringChannel(messageContext.getGuild(), channel, messageContext)) {
+                    MessageSender.sendMessage(
+                            "Registered " + channel.getAsMention() + " as monitoring Channel",
+                            messageContext.getChannel());
+                }
+            } else {
+                MessageSender.sendSimpleError(ErrorType.INVALID_CHANNEL, messageContext.getChannel());
+                return true;
+            }
+        } else {
+            MessageSender.sendSimpleError(ErrorType.INVALID_ID, messageContext.getChannel());
+        }
+        return false;
+    }
+
+    private void remove(String arg, MessageEventDataWrapper messageContext) {
+        try {
+            int i = Integer.parseInt(arg);
+            if (MonitoringData.removeMonitoringAddressByIndex(messageContext.getGuild(), i, messageContext)) {
+                MessageSender.sendMessage("Removed address with index: " + i, messageContext.getChannel());
+            }
+        } catch (NumberFormatException e) {
+            MessageSender.sendSimpleError(ErrorType.NOT_A_NUMBER, messageContext.getChannel());
+        }
+    }
+
+    private boolean easyEnable(MessageEventDataWrapper messageContext) {
+        if (MonitoringData.setMonitoringChannel(messageContext.getGuild(),
+                messageContext.getTextChannel(), messageContext)) {
+            MessageSender.sendMessage(
+                    "Registered " + messageContext.getTextChannel().getAsMention() + " as monitoring Channel",
+                    messageContext.getChannel());
+            return true;
+        }
+        return false;
+    }
+
+    private void disable(MessageEventDataWrapper messageContext) {
+        MonitoringData.removeMonitoringChannel(messageContext.getGuild(), messageContext);
+        MessageSender.sendMessage("Monitoring Channel removed!", messageContext.getChannel());
+    }
+
+    private void show(MessageEventDataWrapper messageContext) {
+        List<Address> monitoringAddresses = MonitoringData.getMonitoringAddressesForGuild(
+                messageContext.getGuild(), messageContext);
+
+        String[][] preparedStringTable = TextFormatting.getPreparedStringTable(monitoringAddresses,
+                "Index", "Name", "Address", "Minecraft");
+
+        for (int i = 1; i < preparedStringTable.length; i++) {
+            Address address = monitoringAddresses.get(i - 1);
+            preparedStringTable[i] = new String[] {
+                    address.getId() + "",
+                    address.getName(),
+                    address.getAddress(),
+                    TextFormatting.mapBooleanTo(address.isMinecraftIp(), "yes", "no")};
+        }
+
+        String asTable = TextFormatting.getAsTable(preparedStringTable);
+
+        MessageSender.sendMessage("Registered for Monitoring: " + lineSeparator() + asTable,
+                messageContext.getChannel());
     }
 }
