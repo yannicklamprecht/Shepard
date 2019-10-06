f3d8f40232048ffe047ccc88771c52ad4381a378
Florian Fülling <fuelling@mapegy.com>
2019-10-06 17:55:01 +0200

    remove redundant catch block and add delete function rename to say

---
+++ src/main/java/de/eldoria/shepard/contexts/commands/fun/Say.java
@@ -0,0 +1,42 @@
+package de.eldoria.shepard.contexts.commands.fun;
+
+import de.eldoria.shepard.wrapper.MessageEventDataWrapper;
+import de.eldoria.shepard.messagehandler.MessageSender;
+import de.eldoria.shepard.contexts.commands.Command;
+import de.eldoria.shepard.contexts.commands.CommandArg;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+
+public class Say extends Command {
+
+    /**
+     * Creates a new Sayd command object.
+     */
+    public Say() {
+        commandName = "say";
+        commandDesc = "Let shepard say something - Use \"sayd\" to delete your command afterwards.";
+        commandArgs = new CommandArg[]
+                {new CommandArg("Message", "Message Shepard should say.", true),};
+        commandAliases = new String[] {"sayd"};
+    }
+
+    @Override
+    protected void internalExecute(String label, String[] args, MessageEventDataWrapper messageContext) {
+        if (args.length == 0) {
+            MessageSender.sendError(
+                    new MessageEmbed.Field[] {
+                            new MessageEmbed.Field("Too few arguments",
+                                    "Use \"&help sayd\" for more information", false)},
+                    messageContext.getChannel());
+        } else {
+            String message = "";
+            for (String arg : args) {
+                message = message.concat(arg + " ");
+            }
+            MessageSender.sendMessage(message, messageContext.getChannel());
+        }
+
+        if (label.equalsIgnoreCase("sayd")) {
+            messageContext.getMessage().delete().queue();
+        }
+    }
+}
