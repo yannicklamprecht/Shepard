ba420cd3dea94a7612b099a80d5fd81430fa6864
Florian Fülling <fuelling@mapegy.com>
2019-10-06 18:02:48 +0200

    move dublicated code to own class

---
+++ src/main/java/de/eldoria/shepard/contexts/commands/admin/PrivateMessageHelper.java
@@ -0,0 +1,23 @@
+package de.eldoria.shepard.contexts.commands.admin;
+
+import de.eldoria.shepard.messagehandler.ErrorType;
+import de.eldoria.shepard.messagehandler.MessageSender;
+import de.eldoria.shepard.util.Emoji;
+import de.eldoria.shepard.util.TextFormatting;
+import de.eldoria.shepard.wrapper.MessageEventDataWrapper;
+import net.dv8tion.jda.api.entities.User;
+
+class PrivateMessageHelper {
+    static void sendPrivateMessage(String[] args, MessageEventDataWrapper messageContext, User user) {
+        if (user == null) {
+            MessageSender.sendSimpleError(ErrorType.INVALID_USER, messageContext.getChannel());
+            return;
+        }
+
+        String text = TextFormatting.getRangeAsString(" ", args, 1, args.length);
+
+        MessageSender.sendMessage(user, messageContext.getMessage().getAttachments(), text, messageContext);
+
+        messageContext.getMessage().addReaction(Emoji.CHECK_MARK_BUTTON.unicode).queue();
+    }
+}
