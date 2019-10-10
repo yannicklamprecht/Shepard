d6ebadc5882235f766509c6d8b05abe05ee3eac2
Florian Fülling <mail@code-fox.de>
2019-10-10 13:28:38 +0200

    reverted ContextSensitiveChanges

--- src/main/java/de/eldoria/shepard/contexts/ContextSensitive.java
+++ src/main/java/de/eldoria/shepard/contexts/ContextSensitive.java
@@ -21,7 +21,7 @@ import java.util.Map;
 /**
  * A {@link ListenerAdapter} filtering events for different contexts.
  */
-public abstract class ContextSensitive extends Thread {
+public abstract class ContextSensitive {
 
     /**
      * Creates a new context Sensitive event.
