e5c2f2ae0dab0cd1f934de32837b830d5bf6fef9
Florian Fülling <mail@code-fox.de>
2019-10-19 05:12:47 +0200

    changed context register

--- src/main/java/de/eldoria/shepard/register/ContextRegister.java
+++ src/main/java/de/eldoria/shepard/register/ContextRegister.java
@@ -1,7 +1,6 @@
 package de.eldoria.shepard.register;
 
-import de.eldoria.shepard.contexts.ContextSensitive;
-import de.eldoria.shepard.contexts.commands.admin.BotPresence;
+import de.eldoria.shepard.contexts.commands.botconfig.BotPresence;
 import de.eldoria.shepard.contexts.commands.admin.Changelog;
 import de.eldoria.shepard.contexts.commands.admin.Greeting;
 import de.eldoria.shepard.contexts.commands.admin.Invite;
@@ -20,19 +19,8 @@ import de.eldoria.shepard.contexts.commands.botconfig.ManageContextGuild;
 import de.eldoria.shepard.contexts.commands.botconfig.ManageContextUsers;
 import de.eldoria.shepard.contexts.commands.exklusive.IsHaddeWorking;
 import de.eldoria.shepard.contexts.commands.exklusive.Meetings;
-import de.eldoria.shepard.contexts.commands.fun.GuessGame;
+import de.eldoria.shepard.contexts.commands.fun.*;
 import de.eldoria.shepard.contexts.commands.admin.GuessGameConfig;
-import de.eldoria.shepard.contexts.commands.fun.LargeEmote;
-import de.eldoria.shepard.contexts.commands.fun.MagicConch;
-import de.eldoria.shepard.contexts.commands.fun.MockingSpongebob;
-import de.eldoria.shepard.contexts.commands.fun.Oha;
-import de.eldoria.shepard.contexts.commands.fun.Owo;
-import de.eldoria.shepard.contexts.commands.fun.Quote;
-import de.eldoria.shepard.contexts.commands.fun.RandomJoke;
-import de.eldoria.shepard.contexts.commands.fun.Kudos;
-import de.eldoria.shepard.contexts.commands.fun.Say;
-import de.eldoria.shepard.contexts.commands.fun.Someone;
-import de.eldoria.shepard.contexts.commands.fun.Uwu;
 import de.eldoria.shepard.contexts.commands.util.GetRaw;
 import de.eldoria.shepard.contexts.commands.util.Help;
 import de.eldoria.shepard.contexts.commands.util.HireMe;
@@ -55,11 +43,82 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class ContextRegister {
-    private static ContextRegister instance;
-
-    private final List<ContextSensitive> contextSensitives = new ArrayList<>();
 
     private ContextRegister() {
+    }
+
+    private static void registerKeywords() {
+        new AmIRight();
+        new CommanderQuestion();
+        new Communism();
+        new DariNope();
+        new DariYes();
+        new Mlp();
+        new Normandy();
+        new Nudes();
+        new Thing();
+        new SomeoneKeyword();
+    }
+
+    private static void registerUtilCommands() {
+        new GetRaw();
+        new Help();
+        new HireMe();
+        new ListServer();
+        new Test();
+        new UserInfo();
+        new Home();
+    }
+
+    private static void registerFunCommands() {
+        new MagicConch();
+        new MockingSpongebob();
+        new Oha();
+        new Owo();
+        new RandomJoke();
+        new Say();
+        new Uwu();
+        new Quote();
+        new Someone();
+        new LargeEmote();
+        new GuessGame();
+        new Kudos();
+        new KudoLottery();
+    }
+
+    private static void registerExclusiveCommands() {
+        new IsHaddeWorking();
+        new Meetings();
+    }
+
+    private static void registerBotConfigCommands() {
+        new ContextInfo();
+        new ManageContext();
+        new ManageContextGuild();
+        new ManageContextUsers();
+    }
+
+    private static void registerAdminCommands() {
+        new Greeting();
+        new Invite();
+        new Prefix();
+        new ShowKeyword();
+        new TicketSettings();
+        new Ticket();
+        new Changelog();
+        new ManageQuote();
+        new Permission();
+        new GuessGameConfig();
+        new BotPresence();
+        new PrivateAnswer();
+        new SendPrivateMessage();
+        new Monitoring();
+    }
+
+    /**
+     * Registers all contexts in command or keyword collection.
+     */
+    public static void registerContexts() {
         registerBotConfigCommands();
         registerAdminCommands();
         registerExclusiveCommands();
@@ -67,85 +126,5 @@ public final class ContextRegister {
         registerFunCommands();
 
         registerKeywords();
-    }
-
-    private void registerKeywords() {
-        contextSensitives.add(new AmIRight());
-        contextSensitives.add(new CommanderQuestion());
-        contextSensitives.add(new Communism());
-        contextSensitives.add(new DariNope());
-        contextSensitives.add(new DariYes());
-        contextSensitives.add(new Mlp());
-        contextSensitives.add(new Normandy());
-        contextSensitives.add(new Nudes());
-        contextSensitives.add(new Thing());
-        contextSensitives.add(new SomeoneKeyword());
-    }
-
-    private void registerUtilCommands() {
-        contextSensitives.add(new GetRaw());
-        contextSensitives.add(new Help());
-        contextSensitives.add(new HireMe());
-        contextSensitives.add(new ListServer());
-        contextSensitives.add(new Test());
-        contextSensitives.add(new UserInfo());
-        contextSensitives.add(new Home());
-    }
-
-    private void registerFunCommands() {
-        contextSensitives.add(new MagicConch());
-        contextSensitives.add(new MockingSpongebob());
-        contextSensitives.add(new Oha());
-        contextSensitives.add(new Owo());
-        contextSensitives.add(new RandomJoke());
-        contextSensitives.add(new Say());
-        contextSensitives.add(new Uwu());
-        contextSensitives.add(new Quote());
-        contextSensitives.add(new Someone());
-        contextSensitives.add(new LargeEmote());
-        contextSensitives.add(new GuessGame());
-        contextSensitives.add(new Kudos());
-    }
-
-    private void registerExclusiveCommands() {
-        contextSensitives.add(new IsHaddeWorking());
-        contextSensitives.add(new Meetings());
-    }
-
-    private void registerBotConfigCommands() {
-        contextSensitives.add(new ContextInfo());
-        contextSensitives.add(new ManageContext());
-        contextSensitives.add(new ManageContextGuild());
-        contextSensitives.add(new ManageContextUsers());
-    }
-
-    private void registerAdminCommands() {
-        contextSensitives.add(new Greeting());
-        contextSensitives.add(new Invite());
-        contextSensitives.add(new Prefix());
-        contextSensitives.add(new ShowKeyword());
-        contextSensitives.add(new TicketSettings());
-        contextSensitives.add(new Ticket());
-        contextSensitives.add(new Changelog());
-        contextSensitives.add(new ManageQuote());
-        contextSensitives.add(new Permission());
-        contextSensitives.add(new GuessGameConfig());
-        contextSensitives.add(new BotPresence());
-        contextSensitives.add(new PrivateAnswer());
-        contextSensitives.add(new SendPrivateMessage());
-        contextSensitives.add(new Monitoring());
-    }
-
-    private static void getInstance() {
-        if (instance == null) {
-            instance = new ContextRegister();
-        }
-    }
-
-    /**
-     * Registers all contexts in command or keyword collection.
-     */
-    public static void registerContexts() {
-        getInstance();
     }
 }
