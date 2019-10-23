30e89108c9a7e07255e54cd0173048ec761ded5c
Florian Fülling <mail@code-fox.de>
2019-10-23 22:11:20 +0200

    add unit tests

---
+++ src/test/java/de/eldoria/shepard/util/TextFormattingTest.java
@@ -0,0 +1,98 @@
+package de.eldoria.shepard.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TextFormattingTest {
+
+    @Test
+    void fillString() {
+        String test = TextFormatting.fillString("test", 5);
+        assertEquals(5, test.length());
+    }
+
+    @Test
+    void getRangeAsString0() {
+        String[] array = new String[] {"this", "is", "a", "simple", "test", "text", "as", "array"};
+        String rangeAsString = TextFormatting.getRangeAsString(" ", array, 1, 8);
+        assertEquals(String.join(" ", new String[] {"is", "a", "simple", "test", "text", "as", "array"}), rangeAsString);
+    }
+
+    @Test
+    void getRangeAsString1() {
+        String[] array = new String[] {"this", "is", "a", "simple", "test", "text", "as", "array"};
+        String rangeAsString = TextFormatting.getRangeAsString(" ", array, 1, 0);
+        assertEquals(String.join(" ", new String[] {"is", "a", "simple", "test", "text", "as", "array"}), rangeAsString);
+    }
+
+    @Test
+    void getRangeAsString2() {
+        String[] array = new String[] {"this", "is", "a", "simple", "test", "text", "as", "array"};
+        String rangeAsString = TextFormatting.getRangeAsString(" ", array, 1, -1);
+        assertEquals(String.join(" ", new String[] {"is", "a", "simple", "test", "text", "as"}), rangeAsString);
+    }
+
+    @Test
+    void getRangeAsString3() {
+        String[] array = new String[] {"this", "is", "a", "simple", "test", "text", "as", "array"};
+        String rangeAsString = TextFormatting.getRangeAsString(" ", array, -3, 0);
+        assertEquals(String.join(" ", new String[] {"text", "as", "array"}), rangeAsString);
+    }
+
+    @Test
+    void getRangeAsString4() {
+        String[] array = new String[] {"this", "is", "a", "simple", "test", "text", "as", "array"};
+        String rangeAsString = TextFormatting.getRangeAsString(" ", array, 3, 4);
+        assertEquals("simple", rangeAsString);
+    }
+
+    @Test
+    void getRangeAsString5() {
+        String[] array = new String[] {"this", "is", "a", "simple", "test", "text", "as", "array"};
+        String rangeAsString = TextFormatting.getRangeAsString(" ", array, -3, -2);
+        assertEquals("text", rangeAsString);
+    }
+
+    @Test
+    void getRangeAsString6() {
+        String[] array = new String[] {"this", "is", "a", "simple", "test", "text", "as", "array"};
+        String rangeAsString = TextFormatting.getRangeAsString(" ", array, -10, -11);
+        assertEquals("", rangeAsString);
+    }
+
+    @Test
+    void getRangeAsString7() {
+        String[] array = new String[] {"this", "is", "a", "simple", "test", "text", "as", "array"};
+        String rangeAsString = TextFormatting.getRangeAsString(" ", array, 30, 35);
+        assertEquals("", rangeAsString);
+    }
+
+    @Test
+    void trimText0() {
+        String text = "This is a simple text to test text cropping";
+        String cropText = TextFormatting.cropText(text, "...", text.length(), false);
+        assertEquals(text, cropText);
+    }
+
+    @Test
+    void trimText1() {
+        String text = "This is a simple text to test text cropping";
+        String cropText = TextFormatting.cropText(text, "...", text.length(), false);
+        assertEquals(text, cropText);
+    }
+
+    @Test
+    void trimText2() {
+        String text = "This is a simple text to test text cropping";
+        String cropText = TextFormatting.cropText(text, "...", text.length() - 1, false);
+        assertEquals("This is a simple text to test text crop...", cropText);
+    }
+
+    @Test
+    void trimText3() {
+        String text = "This is a simple text to test text cropping";
+        String cropText = TextFormatting.cropText(text, "...", text.length() - 1, true);
+        assertEquals("This is a simple text to test text...", cropText);
+    }
+}
---
+++ src/test/java/de/eldoria/shepard/util/VerifierTest.java
@@ -0,0 +1,106 @@
+package de.eldoria.shepard.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VerifierTest {
+
+    @Test
+    void isValidId0() {
+        assertTrue(Verifier.isValidId("123456789123456789")
+                && Verifier.isValidId("<@123456789123456789>")
+                && Verifier.isValidId("<#123456789123456789>")
+                && Verifier.isValidId("<&123456789123456789>")
+                && Verifier.isValidId("<@#123456789123456789>")
+                && Verifier.isValidId("<@!123456789123456789>")
+                && Verifier.isValidId("<!123456789123456789>"));
+    }
+
+    @Test
+    void isValidId1() {
+        assertFalse(Verifier.isValidId("1234567891234567891")
+                && Verifier.isValidId("12345678912345678")
+                && Verifier.isValidId("<&123456789123456789>")
+                && Verifier.isValidId("<?123456789123456789>"));
+    }
+
+    @Test
+    void checkAndGetBoolean0() {
+        assertFalse(Verifier.checkAndGetBoolean("false").stateAsBoolean);
+    }
+    @Test
+    void checkAndGetBoolean1() {
+        assertFalse(Verifier.checkAndGetBoolean("tru").stateAsBoolean);
+    }
+    @Test
+    void checkAndGetBoolean2() {
+        assertTrue(Verifier.checkAndGetBoolean("true").stateAsBoolean);
+    }
+
+    @Test
+    void getAddressType0() {
+        AddressType addressType = Verifier.getAddressType("1.1.1.1");
+        assertEquals(AddressType.IPV4, addressType);
+    }
+
+    @Test
+    void getAddressType1() {
+        AddressType addressType = Verifier.getAddressType("123.123.123.123:12345");
+        assertEquals(AddressType.IPV4, addressType);
+    }
+
+    @Test
+    void getAddressType2() {
+        AddressType addressType = Verifier.getAddressType("1.1.1:1");
+        assertEquals(AddressType.NONE, addressType);
+    }
+
+    @Test
+    void getAddressType3() {
+        AddressType addressType = Verifier.getAddressType("1.1.1.1:123456");
+        assertEquals(AddressType.NONE, addressType);
+    }
+
+    @Test
+    void getAddressType4() {
+        AddressType addressType = Verifier.getAddressType("740:5e2d:6fcb:ec99:3b7c:fdd5:af77:d693");
+        assertEquals(AddressType.IPV6, addressType);
+    }
+
+    @Test
+    void getAddressType5() {
+        AddressType addressType = Verifier.getAddressType("e7f1:4046:9c9c:6acd:2fcd:479c:3ff8:44f3");
+        assertEquals(AddressType.IPV6, addressType);
+    }
+
+    @Test
+    void getAddressType6() {
+        AddressType addressType = Verifier.getAddressType("e7f1:4046:9c9c:6acd::::44f3");
+        assertEquals(AddressType.IPV6, addressType);
+    }
+
+    @Test
+    void getAddressType7() {
+        AddressType addressType = Verifier.getAddressType("[e7f1:4046:9c9c:6acd::::44f3]:12345");
+        assertEquals(AddressType.IPV6, addressType);
+    }
+
+    @Test
+    void getAddressType8() {
+        AddressType addressType = Verifier.getAddressType("e7f1:4046:9c9c:6acd::::44f3]:12345");
+        assertEquals(AddressType.NONE, addressType);
+    }
+
+    @Test
+    void getAddressType9() {
+        AddressType addressType = Verifier.getAddressType("[e7f1:4046:9c9c:6acd::::44f3:12345");
+        assertEquals(AddressType.NONE, addressType);
+    }
+
+    @Test
+    void getAddressType10() {
+        AddressType addressType = Verifier.getAddressType("[e7f1:4046:9c9c:6acd::::44f3]:123456");
+        assertEquals(AddressType.NONE, addressType);
+    }
+}
