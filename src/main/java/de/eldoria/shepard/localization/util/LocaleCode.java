1a66a529b4435880d50263c9dcfb84d40ad4f697
Florian Fülling <mail@code-fox.de>
2019-11-05 20:01:07 +0100

    added language name

--- src/main/java/de/eldoria/shepard/localization/util/LocaleCode.java
+++ src/main/java/de/eldoria/shepard/localization/util/LocaleCode.java
@@ -4,24 +4,29 @@ public enum LocaleCode {
     /**
      * English language code.
      */
-    EN_US("en_US"),
+    EN_US("en_US", "English"),
     /**
      * German language code.
      */
-    DE_DE("de_DE");
+    DE_DE("de_DE", "Deutsch");
 
     /**
      * Get the locale code with pattern [a-z]{2}_[A-Z]{2}.
      */
     public final String code;
+    public final String languageName;
 
-    LocaleCode(String code) {
+    LocaleCode(String code, String languageName) {
         this.code = code;
+        this.languageName = languageName;
     }
 
     public static LocaleCode parse(String codeString) {
         for (LocaleCode code : LocaleCode.values()) {
             if (code.code.equalsIgnoreCase(codeString)) {
+                return code;
+            }
+            if (code.languageName.equalsIgnoreCase(codeString)) {
                 return code;
             }
         }
