e5086fbf115f3b5c81be078a588d87036fd11be6
Florian Fülling <mail@code-fox.de>
2019-11-08 21:11:06 +0100

    emoji and locale is now seperated

--- src/main/java/de/eldoria/shepard/contexts/ContextCategory.java
+++ src/main/java/de/eldoria/shepard/contexts/ContextCategory.java
@@ -6,27 +6,27 @@ public enum ContextCategory {
     /**
      * Context is a command of category bot administration.
      */
-    ADMIN("\u2699 " + HelpLocale.M_ADMIN),
+    ADMIN("\u2699", HelpLocale.M_ADMIN.tag),
     /**
      * Context is a command of category bot configuration.
      */
-    BOT_CONFIG("\uD83D\uDD27 " + HelpLocale.M_BOT_CONFIG),
+    BOT_CONFIG("\uD83D\uDD27", HelpLocale.M_BOT_CONFIG.tag),
     /**
      * Context is a command of category server exclusive.
      */
-    EXCLUSIVE("\uD83C\uDF89 " + HelpLocale.M_EXCLUSIVE),
+    EXCLUSIVE("\uD83C\uDF89", HelpLocale.M_EXCLUSIVE.tag),
     /**
      * Context is a command of category entertainment.
      */
-    FUN("\uD83D\uDD79 " + HelpLocale.M_FUN),
+    FUN("\uD83D\uDD79", HelpLocale.M_FUN.tag),
     /**
      * Context is a command of category utility.
      */
-    UTIL("\u2049 " + HelpLocale.M_UTIL),
+    UTIL("\u2049", HelpLocale.M_UTIL.tag),
     /**
      * Context is keyword.
      */
-    KEYWORD("Keywords");
+    KEYWORD("", "Keywords");
 
     /**
      * Formatted name of the category.
@@ -38,7 +38,7 @@ public enum ContextCategory {
      *
      * @param tag locale tag of the category
      */
-    ContextCategory(String tag) {
-        this.categoryName = tag;
+    ContextCategory(String emoji, String tag) {
+        this.categoryName = emoji + " " + tag;
     }
 }
