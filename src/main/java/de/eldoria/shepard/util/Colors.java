e5bfb186b828105fdf277a0d6599b2ebca9c4a09
Florian Fülling <mail@code-fox.de>
2019-12-23 16:29:58 +0100

    add class for color constants

---
+++ src/main/java/de/eldoria/shepard/util/Colors.java
@@ -0,0 +1,34 @@
+package de.eldoria.shepard.util;
+
+import java.awt.*;
+
+public final class Colors {
+    public static final Color pastel_light_red = new Color(250, 132, 132);
+    public static final Color pastel_red = new Color(245, 93, 93);
+    public static final Color pastel_dark_red = new Color(168, 69, 69);
+    public static final Color pastel_orange = new Color(237, 156, 85);
+    public static final Color pastel_yellow = new Color(237, 221, 104);
+    public static final Color pastel_light_green = new Color(188, 242, 141);
+    public static final Color pastel_green = new Color(137, 237, 123);
+    public static final Color pastel_dark_green = new Color(83, 158, 73);
+    public static final Color pastel_light_blue = new Color(132, 204, 240);
+    public static final Color pastel_blue = new Color(132, 161, 240);
+    public static final Color pastel_dark_blue = new Color(132, 161, 240);
+    public static final Color pastel_purple = new Color(197, 141, 242);
+    public static final Color pastel_pink = new Color(238, 122, 240);
+    public static final Color pastel_light_pink = new Color(240, 122, 185);
+    public static final Color strong_light_red = new Color(255, 97, 97);
+    public static final Color strong_red = new Color(255, 0, 0);
+    public static final Color strong_dark_red = new Color(176, 0, 0);
+    public static final Color strong_orange = new Color(255, 132, 0);
+    public static final Color strong_yellow = new Color(255, 255, 0);
+    public static final Color strong_light_green = new Color(74, 255, 95);
+    public static final Color strong_green = new Color(0, 255, 30);
+    public static final Color strong_dark_green = new Color(0, 181, 21);
+    public static final Color strong_light_blue = new Color(102, 224, 255);
+    public static final Color strong_blue = new Color(0, 0, 255);
+    public static final Color strong_dark_blue = new Color(0, 0, 191);
+    public static final Color strong_purple = new Color(204, 0, 255);
+    public static final Color strong_pink = new Color(255, 0, 255);
+    public static final Color strong_light_pink = new Color(255, 102, 255);
+}
