f7f8d19b632cbeece4474d67281e3db54f8d9422
Florian Fülling <mail@code-fox.de>
2020-02-09 22:05:29 +0100

    removed auto config creation

--- src/main/java/de/eldoria/shepard/configuration/Loader.java
+++ src/main/java/de/eldoria/shepard/configuration/Loader.java
@@ -7,11 +7,7 @@ import org.yaml.snakeyaml.constructor.Constructor;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -20,111 +16,21 @@ import java.nio.file.Paths;
  */
 @Slf4j
 public final class Loader {
-    private static Loader loader;
+    private Loader() { }
 
-    private Config config;
-
-    private Loader() {
-        reloadConfig();
-    }
-
-    /**
-     * Get config loader instance.
-     *
-     * @return return Loader instance
-     */
-    public static Loader getConfigLoader() {
-        if (loader == null) {
-            loader = new Loader();
-        }
-        return loader;
-    }
-
-    /**
-     * Get config.
-     *
-     * @return config object
-     */
-    public Config getConfig() {
-        return config;
-    }
-
-    private void reloadConfig() {
+    public static Config loadConfig() throws FileNotFoundException {
         File shepardJar = new File(".");
         File shepardFolder = shepardJar.getAbsoluteFile().getParentFile();
         String home = shepardFolder.toString();
 
-        Path configDir = Paths.get(home, "/config");
-
-        if (!Files.exists(configDir)) {
-            log.info("{} not found. Trying to create", configDir);
-            try {
-                Files.createDirectory(configDir);
-            } catch (IOException e) {
-                log.error("Directory for config could not be created!", e);
-                return;
-            }
-        } else {
-            log.info("Config directory found!");
-        }
-
-        Path configFile = Paths.get(configDir + "/config.yml");
-
-        if (!Files.exists(configFile)) {
-            log.info("Config file not found! Trying to create a default config!");
-
-            try {
-                Files.createFile(configFile);
-            } catch (IOException e) {
-                log.error("Could not create config file", e);
-            }
-            InputStream systemResource = getClass().getClassLoader().getResourceAsStream("config.yml");
-            if (systemResource == null) {
-                return;
-            }
-
-            log.info("Loading config template!");
-
-            OutputStream os = null;
-            try {
-                os = new FileOutputStream(configFile.toFile());
-                byte[] buffer = new byte[1024];
-                int length;
-                while ((length = systemResource.read(buffer)) > 0) {
-                    os.write(buffer, 0, length);
-                }
-
-            } catch (IOException e) {
-                log.error("failed to read config file", e);
-            } finally {
-                try {
-                    systemResource.close();
-                    if (os != null) {
-                        os.close();
-                    }
-
-                } catch (IOException e) {
-                    log.error("", e);
-                }
-            }
-
-            log.info("Config file created!");
-        }
+        Path configFile = Paths.get(home + "/" + System.getProperty("shepard.config"));
 
         InputStream inputStream;
         Yaml yaml = new Yaml(new Constructor(Config.class));
-        try {
-            inputStream = new FileInputStream(configFile.toString());
-        } catch (FileNotFoundException e) {
-            log.error("File not found!", e);
-            return;
-        }
-        try {
-            this.config = yaml.load(inputStream);
-        } catch (RuntimeException e) {
-            log.error("failed to load yml config", e);
-            return;
-        }
+
+        inputStream = new FileInputStream(configFile.toString());
+
         log.info("Config loaded");
+        return yaml.load(inputStream);
     }
 }
