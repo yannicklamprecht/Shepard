2f08a005f7c7de4bc87ef9c8b378037a5c3d9c53
RainbowDashLabs <mail@code-fox.de>
2020-04-09 01:38:35 +0200

    add mass effect quote command

---
+++ src/main/java/de/eldoria/shepard/contexts/commands/fun/MassEffect.java
@@ -0,0 +1,73 @@
+package de.eldoria.shepard.contexts.commands.fun;
+
+import de.eldoria.shepard.contexts.ContextCategory;
+import de.eldoria.shepard.contexts.commands.ArgumentParser;
+import de.eldoria.shepard.contexts.commands.Command;
+import de.eldoria.shepard.localization.enums.commands.fun.MassEffectLocale;
+import de.eldoria.shepard.messagehandler.ErrorType;
+import de.eldoria.shepard.messagehandler.MessageSender;
+import de.eldoria.shepard.wrapper.MessageEventDataWrapper;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.EmbedBuilder;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class MassEffect extends Command {
+    List<String> quotes;
+
+    public MassEffect() {
+        commandName = "massEffect";
+        commandAliases = new String[] {"shepQuote"};
+        commandDesc = MassEffectLocale.DESCRIPTION.tag;
+        category = ContextCategory.FUN;
+
+        Yaml yaml = new Yaml(new Constructor(QuoteList.class));
+
+        quotes = new ArrayList<>();
+        try {
+            QuoteList quoteList = yaml.load(getClass().getResourceAsStream("/mass_effect_1.yml"));
+            quotes.addAll(Arrays.asList(quoteList.quotes));
+            quoteList = yaml.load(getClass().getResourceAsStream("/mass_effect_2.yml"));
+            quotes.addAll(Arrays.asList(quoteList.quotes));
+            quoteList = yaml.load(getClass().getResourceAsStream("/mass_effect_3.yml"));
+            quotes.addAll(Arrays.asList(quoteList.quotes));
+        } catch (Exception e) {
+            log.error("Could not load mass effect quotes", e);
+        }
+        log.info("Loaded " + quotes.size() + " mass effect quotes.");
+    }
+
+    @Override
+    protected void internalExecute(String label, String[] args, MessageEventDataWrapper messageContext) {
+        List<String> filteredQuotes;
+        if (args.length != 0) {
+            filteredQuotes = quotes.stream()
+                    .filter(q -> q.toLowerCase().contains(ArgumentParser.getMessage(args, 0).toLowerCase()))
+                    .collect(Collectors.toList());
+            if (filteredQuotes.size() == 0) {
+                MessageSender.sendSimpleError(ErrorType.NO_QUOTE_FOUND, messageContext.getTextChannel());
+                return;
+            }
+        } else {
+            filteredQuotes = quotes;
+        }
+
+        EmbedBuilder builder = new EmbedBuilder()
+                .setTitle("Mass Effect Quote")
+                .setDescription(filteredQuotes.get(new Random().nextInt(filteredQuotes.size())));
+        messageContext.getChannel().sendMessage(builder.build()).queue();
+    }
+
+    @Data
+    public static class QuoteList {
+        private String[] quotes = null;
+    }
+}
