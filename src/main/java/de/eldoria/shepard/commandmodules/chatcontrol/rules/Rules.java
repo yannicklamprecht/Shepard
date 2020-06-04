35ea93b4db2fe1309abdd87d686b8c40eafa3498
RainbowDashLabs <mail@code-fox.de>
2020-06-04 22:21:21 +0200

    add rules

--- src/main/java/de/eldoria/shepard/commandmodules/chatcontrol/rules/Rules.java
+++ src/main/java/de/eldoria/shepard/commandmodules/chatcontrol/rules/Rules.java
@@ -5,17 +5,28 @@ import net.dv8tion.jda.api.entities.Message;
 import java.util.function.BiFunction;
 
 public enum Rules {
-    NORMAL_MESSAGE, LINK_MESSAGE;
+    NORMAL_MESSAGE(checkNormal()),
+    LINK_MESSAGE(checkNormal()),
+    MAX_MENTIONS(checkNormal()),
+    ATTACHMENT_MESSAGE(checkNormal()),
+    CAPS_MESSAGES(checkNormal()),
+    DOUBLE_MESSAGES(checkNormal()),
+    BADWORD_MESSAGES(checkNormal()),
+    REGEX_MESSAGES(checkNormal());
 
     private final BiFunction<Message, RuleSettings, Boolean> checkFunction;
 
     Rules(BiFunction<Message, RuleSettings, Boolean> checkFunction) {
-
         this.checkFunction = checkFunction;
     }
 
-    public boolean isNotAllowed(Message message, RuleSettings ruleSettings){
-        return this.checkFunction(message, ruleSettings);
+    public boolean isNotAllowed(Message message, RuleSettings ruleSettings) {
+        return this.checkFunction.apply(message, ruleSettings);
     }
 
+    public static BiFunction<Message, RuleSettings, Boolean> checkNormal() {
+        return (message, rules) ->{
+            return true;
+        };
+    }
 }
