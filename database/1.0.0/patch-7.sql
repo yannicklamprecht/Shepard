db4c16a175c9f7f3d1e30401b427304e7d7a5598
Unbekannt <unbekannt-gitlab@protonmail.com>
2020-06-05 01:05:53 +0200

    ModLog + DB Functions for Bad_words

---
+++ database/1.0.0/patch-7.sql
@@ -0,0 +1,99 @@
+CREATE TABLE IF NOT EXISTS shepard_data.modlog
+(
+    guild_id bigint NOT NULL,
+    channel_id bigint,
+    CONSTRAINT modlog_pkey PRIMARY KEY (guild_id)
+);
+CREATE OR REPLACE FUNCTION shepard_func.set_modLog(
+	_guild_id bigint,
+	_channel_id bigint)
+    RETURNS void
+    LANGUAGE 'plpgsql'
+
+    COST 100
+    VOLATILE
+
+AS
+$BODY$
+    INSERT INTO shepard_data.modlog (guild_id, channel_id)
+    VALUES (_guild_id, _channel_id)
+    ON CONFLICT (guild_id)
+        DO UPDATE
+            set channel_id = _channel_id
+$BODY$;
+CREATE OR REPLACE FUNCTION shepard_data.delete_modlog(_guild_id bigint) RETURNS void
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+
+  DELETE FROM shepard_data.modlog WHERE guild_id = _guild_id;
+
+END
+$$;
+CREATE OR REPLACE FUNCTION shepard_func.get_modlog(_guild_id bigint) RETURNS BIGINT
+    LANGUAGE plpgsql
+AS
+$BODY$
+DECLARE
+    result BIGINT;
+BEGIN
+	SELECT channel_id
+	INTO result
+	FROM shepard_data.modlog
+	WHERE guild_id = _guild_id;
+
+	RETURN result;
+END
+$BODY$;
+CREATE TABLE shepard_data.badwords
+(
+    guild_id bigint NOT NULL,
+    badwords character varying[] COLLATE pg_catalog."default",
+    CONSTRAINT badwords_pkey PRIMARY KEY (guild_id)
+);
+CREATE OR REPLACE FUNCTION shepard_func.get_badwords(
+	_guild_id bigint)
+    RETURNS varchar[]
+    LANGUAGE 'plpgsql'
+
+    COST 100
+    VOLATILE
+
+AS
+$BODY$
+DECLARE
+	result varchar[];
+BEGIN
+SELECT badwords
+INTO result
+FROM shepard_data.badwords
+WHERE guild_id = _guild_id;
+
+RETURN result;
+END
+$BODY$;
+CREATE OR REPLACE FUNCTION shepard_func.add_badword(
+    _guild_id bigint,
+    _word character varying)
+    RETURNS void
+    LANGUAGE 'plpgsql'
+AS $BODY$
+BEGIN
+
+    insert into shepard_data.badwords(guild_id, badwords)
+    VALUES(_guild_id, ARRAY[_word])
+    on CONFLICT (guild_id)
+        DO
+            UPDATE SET badwords =
+                (select
+                     array_agg(dist_words)
+                 from
+                    (SELECT
+                         DISTINCT words as dist_words
+                     from
+                         unnest(array_append(badwords.badwords, _word)) as words)
+                 a);
+
+END
+$BODY$;
