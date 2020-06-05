3b61718b101873d39948cd7556f06bcb29a6115d
Unbekannt <unbekannt-gitlab@protonmail.com>
2020-06-05 17:33:17 +0200

    Prefix List Command

---
+++ src/main/java/de/eldoria/shepard/localization/enums/commands/moderation/RegisterPrefixLocale.java
@@ -0,0 +1,30 @@
+package de.eldoria.shepard.localization.enums.commands.moderation;
+
+public enum RegisterPrefixLocale {
+    DESCRIPTION("command.prefix.subcommand.description"),
+    ADD("command.prefix.subcommand.add"),
+    REMOVE("command.prefix.subcommand.remove"),
+    LIST("command.prefix.subcommand.list"),
+    SUCCESS_ADD("command.prefix.message.success.add"),
+    SUCCESS_REMOVE("command.prefix.message.success.remove"),
+    LIST_TITLE("command.prefix.message.list.title");
+
+    /**
+     * Get the escaped locale code for auto translation.
+     */
+    public final String tag;
+
+    /**
+     * Create a new locale object.
+     *
+     * @param localeCode locale code
+     */
+    RegisterPrefixLocale(String localeCode) {
+        this.tag = "$" + localeCode + "$";
+    }
+
+    @Override
+    public String toString() {
+        return tag;
+    }
+}
