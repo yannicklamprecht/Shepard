e6a72a4dab0149c3b19932a4d9e490cb6461d2d9
RainbowDashLabs <mail@code-fox.de>
2020-06-19 18:06:15 +0200

    fix for possible null value when parsing a flag with a function

--- src/main/java/de/eldoria/shepard/basemodules/commanddispatching/util/FlagParser.java
+++ src/main/java/de/eldoria/shepard/basemodules/commanddispatching/util/FlagParser.java
@@ -2,6 +2,7 @@ package de.eldoria.shepard.basemodules.commanddispatching.util;
 
 import lombok.experimental.UtilityClass;
 
+import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
@@ -28,10 +29,14 @@ public class FlagParser {
      * @param defaultValue value to return if the flag is empty
      * @return the value of the flag or the default value if the flag is not present
      */
+    @Nonnull
     public String getFlagValue(char flag, String[] args, String defaultValue) {
         List<String> argList = new ArrayList<>();
 
+        // set flag as closed
         boolean open = false;
+
+        // iterate over arguments
         for (String arg : args) {
             if (open) {
                 if (arg.length() == 2 && arg.startsWith("-")) {
@@ -43,11 +48,12 @@ public class FlagParser {
                 open = true;
             }
         }
+        // if the flag is not open, the flag wasn't present.
         if (!open) {
             return defaultValue;
         }
         String result = String.join(" ", argList);
-        return result.isEmpty() ? null : result;
+        return result.isEmpty() ? defaultValue : result;
     }
 
     /**
@@ -75,7 +81,6 @@ public class FlagParser {
      */
     public <T> T getFlagValue(Function<String, T> parsingFunction, char flag, String[] args, String defaultValue) {
         String value = getFlagValue(flag, args, defaultValue);
-        if (value == null) return null;
 
         return parsingFunction.apply(value);
     }
